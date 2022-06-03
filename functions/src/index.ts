38da9d3ecf0e377fa4df4f64ea556135cc32eb62
Ansgar Hoyer <ansgar.hoyer@student.hs-rm.de>
2022-06-03 14:09:39 +0200

    fixed linting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,106 +1,118 @@
 import * as functions from "firebase-functions";
-import express = require("express")
-import { body, query, validationResult, } from "express-validator"
-
-const app = express()
+import express = require("express");
+import {body, query, validationResult} from "express-validator";
+import createClient = require("hafas-client");
+import rmvProfile = require("hafas-client/p/rmv");
+const app = express();
 
 const stationsRequestValidation = [
-    body('latitude').isFloat(),
-    body('longitude').isFloat(),
-    body('distance').optional().isInt()
-]
+    body("latitude").isFloat(),
+    body("longitude").isFloat(),
+    body("distance").optional().isInt(),
+];
 
-app.post("/stations", stationsRequestValidation, async (req: express.Request, res: express.Response) => {
+app.post(
+    "/stations",
+    stationsRequestValidation,
+    async (req: express.Request, res: express.Response) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array());
+            }
+            functions.logger.info(req.body);
+            const {latitude, longitude, distance = 400} = req.body;
 
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
+            const client = createClient(rmvProfile, "transport-radar");
+
+            const results = await client.nearby(
+                {type: "location", latitude, longitude},
+                {distance},
+            );
+            functions.logger.info("Hafas Results:", results);
+            return res.json(results);
+        } catch (e) {
+            functions.logger.error(e);
+            return res.status(500).json(e);
         }
-        functions.logger.info(req.body)
-        const { latitude, longitude, distance = 400 } = req.body
+    },
+);
 
-        const createClient = require("hafas-client")
-        const rmvProfile = require("hafas-client/p/rmv")
-        const client = createClient(rmvProfile, "transport-radar")
+const departureRequestValidation = [query("stationID").isNumeric()];
+app.get(
+    "/departures",
+    departureRequestValidation,
+    async (req: express.Request, res: express.Response) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array());
+            }
+            functions.logger.info(req.query);
+            const {stationID} = req.query;
+            if (typeof stationID != "string") {
+                return res.status(400);
+            }
+            const client = createClient(rmvProfile, "transport-radar");
 
-
-        const results = await client.nearby({ type: "location", latitude, longitude }, { distance })
-        functions.logger.info("Hafas Results:", results)
-        return res.json(results)
-
-    } catch (e) {
-        functions.logger.error(e)
-        return res.status(500).json(e)
-    }
-})
-
-const departureRequestValidation = [
-    query('stationID').isNumeric(),
-
-]
-app.get("/departures", departureRequestValidation, async (req: express.Request, res: express.Response) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
+            const results = await client.departures(stationID, undefined);
+            functions.logger.info("Hafas Results:", results);
+            return res.json(results);
+        } catch (e) {
+            functions.logger.error(e);
+            return res.status(500).json(e);
         }
-        functions.logger.info(req.query)
-        const { stationID } = req.query
-
-        const createClient = require("hafas-client")
-        const rmvProfile = require("hafas-client/p/rmv")
-        const client = createClient(rmvProfile, "transport-radar")
-
-
-        const results = await client.departures(stationID)
-        functions.logger.info("Hafas Results:", results)
-        return res.json(results)
-
-    } catch (e) {
-        functions.logger.error(e)
-        return res.status(500).json(e)
-    }
-})
-
+    },
+);
 
 const radarRequestValidation = [
-    body('north').isFloat(),
-    body('west').isFloat(),
-    body('south').isFloat(),
-    body('east').isFloat(),
-    body('duration').isInt().optional(),
-    body('frames').isInt().optional(),
-    body('results').isInt().optional()
-]
+    body("north").isFloat(),
+    body("west").isFloat(),
+    body("south").isFloat(),
+    body("east").isFloat(),
+    body("duration").isInt().optional(),
+    body("frames").isInt().optional(),
+    body("results").isInt().optional(),
+];
 
+app.get(
+    "/radar",
+    radarRequestValidation,
+    async (req: express.Request, res: express.Response) => {
+        functions.logger.debug("radar invoked!");
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                functions.logger.error("invalid request body");
+                return res.status(400).json(errors.array());
+            }
+            functions.logger.info(req.body);
+            const {
+                north,
+                west,
+                south,
+                east,
+                duration = 10,
+                frames = 6,
+                results = 10,
+            } = req.body;
 
-
-app.get("/radar", radarRequestValidation, async (req: express.Request, res: express.Response) => {
-    functions.logger.debug("radar invoked!")
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            functions.logger.error("invalid request body")
-            return res.status(400).json(errors.array())
+            const client = createClient(rmvProfile, "transport-radar");
+            if (client.radar) {
+                const radar_results = await client.radar(
+                    {north, west, south, east},
+                    {duration, frames, results},
+                );
+                functions.logger.info("Hafas Results:", radar_results);
+                return res.json(radar_results);
+            } else {
+                throw Error("Unexpected Error! no Radar available!");
+            }
+        } catch (e) {
+            functions.logger.error(e);
+            return res.status(500).json(e);
         }
-        functions.logger.info(req.body)
-        const { north, west, south, east, duration = 10, frames = 6, results = 10 } = req.body
+    },
+);
 
-        const createClient = require("hafas-client")
-        const rmvProfile = require("hafas-client/p/rmv")
-        const client = createClient(rmvProfile, "transport-radar")
-
-
-        const radar_results = await client.radar({ north, west, south, east }, { duration, frames, results })
-        functions.logger.info("Hafas Results:", radar_results)
-        return res.json(radar_results)
-
-
-    } catch (e) {
-        functions.logger.error(e)
-        return res.status(500).json(e)
-    }
-})
-
-export default functions.https.onRequest(app)
+export default functions.https.onRequest(app);
