d3d2ea43d79dc39191e98d3fd75db1094d2025af
Timothy Tjong-Ayong <timothy.tjong-ayong@student.hs-rm.de>
2022-06-17 12:39:51 +0200

    Update für Form Validation

--- src/pages/Contact/ContactForm.tsx
+++ src/pages/Contact/ContactForm.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { Button, FormControl, FormLabel,   FormErrorMessage,
+import { Button, FormControl, FormLabel, FormErrorMessage,
   FormHelperText, Heading, Input, Textarea, Select, VStack, Flex, Box } from '@chakra-ui/react'
 import Layout from '../../components/Layout/Layout';
 import { Field, Form, Formik, FormikProps } from "formik";
 import { CSSTransition } from 'react-transition-group';
-import "./ContactForm.module.css";
+import * as Yup from 'yup';
+import "./ContactForm.css";
 
 
 interface IFormInputs {
@@ -26,20 +27,40 @@ function ContactForm() {
   );
 
   const [input, setInput] = useState('')
+  const [showForm, setShowForm] = useState(true);
+  const [showMessage, setShowMessage] = useState(false)
 
   const handleInputChange = (e: any) => setInput(e.target.value)
 
   const isError = emailRegex.test(input)
 
+  // function validateEmail(value: string) {
+  //   const valid = emailRegex.test(value)
+  //   let error = ""
+  //   if (!valid) {
+  //    error = 'Bitte gebe deine richtige E-Mail-Adresse an!'     
+  //   } else if (valid) {
+  //     error = ""
+  //   }
+  //   return error
+  // }
+
   function validateEmail(value: string) {
-    const valid = emailRegex.test(value)
-    let error = ""
-    if (!valid) {
-     error = 'Bitte gebe deine richtige E-Mail-Adresse an!'     
-    } else if (valid) {
-      error = ""
+    let error;
+    if (!value) {
+      error = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      error = 'Invalid email address';
     }
-    return error
+    return error;
+  }
+  
+  function validateUsername(value: string) {
+    let error;
+    if (value === '') {
+      error = 'Ey trage hier was ein!';
+    }
+    return error;
   }
 
   const initialValues: IFormInputs = {
@@ -50,46 +71,82 @@ function ContactForm() {
     text: ''
   }
 
+  // const ValidationSchema = Yup.object().shape({
+  //   firstname: Yup.string()
+  //     .min(2, 'Vorname zu kurz!')
+  //     .max(50, 'VorName zu lang!')
+  //     .required('Required'),
+  //   lastname: Yup.string()
+  //     .min(2, 'Nachname zu kurz!')
+  //     .max(50, 'Nachname zu lang!')
+  //     .required('Nachname wird benötigt'),
+  //   email: Yup.string().email('Ungültige email').required('Required'),
+  //   issue
+  //   text: Yup.string()
+  //     .min(2, 'Bitte mindestens 20 Zeichen verwenden.')
+  //     .max(500, 'Bitte maximal 500 Zeichen verwenden.')
+  //     .required('Required'),
+  // });
+
   return (
     <Layout>
       <Flex bg="gray.100" align="center" justify="center" h="100vh">
+
         <CSSTransition
-        in={true}
-        timeout={2000}
+          in={showMessage}
+          timeout={600}
+          classNames="thankyou-message"
+          unmountOnExit
+        >
+          <Heading as='h2' size='2xl'>Vielen Dank für dein Feedback!</Heading>
+        </CSSTransition>   
+        
+        <CSSTransition
+        in={showForm}
+        timeout={600}
         classNames={'contact-form'}
-        appear={true}>
+        appear
+        unmountOnExit
+        onExited={() => setShowMessage(true)}
+        >
           <Box bg="white" p={6} rounded="md" w={600}>
             <Heading as='h2' size='xl'>Kontaktformular</Heading><br/>
             <Formik 
               initialValues={initialValues}
-              onSubmit={console.log}
+              onSubmit={() => {console.log; setShowForm(false)}}
+              // validationSchema={ValidationSchema}
               >
               
-              {(props: FormikProps<IFormInputs>) => (
+              {/* {(props: FormikProps<IFormInputs>) => ( */}
+              {({errors, touched}) => (
+
                 <Form>
                   <VStack spacing={4} align="flex-start">
-                  <h1 id="my-class">TEST ÜBERSCHRIFT</h1>
+                  
                   <FormControl isRequired>
                     <FormLabel htmlFor='firstname'>Vorname</FormLabel>
                     <Field
-                      as={Input}
-                      id='firstname'
-                      placeholder='Vorname'
-                      name='firstname'
-                      // validate={(value: string) => {
-                      //   let error;
-                      //   if (value.length < 2) {
-                      //     error = "Ein Name muss mindestens 2 Buchtaben enthalten";
-                      //   }
-                      //   return error;
-
-                      //}}
-                      />
+                        as={Input}
+                        id='firstname'
+                        placeholder='Vorname'
+                        name='firstname'
+                        validate={validateUsername}
+                        />
+                          
+                    {/* {errors.firstname && touched.firstname && <FormHelperText>{errors.firstname}</FormHelperText>} */}
                   </FormControl>
+                    
+
                   
                   <FormControl isRequired >
                     <FormLabel htmlFor='lastname'>Nachname</FormLabel>
-                    <Field as={Input} id='lastname' name='nachname' placeholder='Nachname'/>
+                    <Field
+                      as={Input}
+                      id='lastname'
+                      name='nachname'
+                      placeholder='Nachname'
+                      validate={validateUsername}
+                    />
                   </FormControl>
 
                   <FormControl isRequired>
@@ -100,8 +157,13 @@ function ContactForm() {
                       type='email'
                       value={input}
                       onChange={handleInputChange}
+                      validate={validateEmail}
+
                     />
+                    {errors.email && touched.email && <div>{errors.email}</div>}
+
                   </FormControl>
+
 
                   <FormControl>
                     <FormLabel htmlFor='issue'>Anliegen</FormLabel>
@@ -121,26 +183,21 @@ function ContactForm() {
                     />
                   </FormControl>
 
-                  <Button  colorScheme='teal' size='md' type="submit">Senden</Button>
+                  <Button
+                    colorScheme='teal'
+                    size='md'
+                    type="submit"
+                  >
+                      Senden
+                  </Button>
                   </VStack>
                 </Form>
               )}
             </Formik>
           </Box>
-          </CSSTransition>
+        </CSSTransition>
       </Flex>
     </Layout>
-
-
-
-    // <form onSubmit={handleSubmit(onSubmit)}>
-    //   <input name="firstname" ref={register} /> {/* register an input */}
-    //   <input name="lastname" ref={register({ required: true })} />
-    //   {errors.lastname && 'Last name is required.'}
-    //   <input name="age" ref={register({ pattern: /\d+/ })} />
-    //   {errors.age && 'Please enter number for age.'}
-    //   <input type="submit" />
-    // </form>
   );
 }
 
