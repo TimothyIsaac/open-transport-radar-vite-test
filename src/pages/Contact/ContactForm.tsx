d42d56384a3ca7ae132a4f253e830d0f3eda2b6b
Timothy Tjong-Ayong <timothy.tjong-ayong@student.hs-rm.de>
2022-06-24 11:58:21 +0200

    🧹code cleanup

--- src/pages/Contact/ContactForm.tsx
+++ src/pages/Contact/ContactForm.tsx
@@ -22,17 +22,9 @@ function ContactForm() {
   const formSubmitHandler: SubmitHandler<IFormInputs> = (data: IFormInputs) => {
     console.log(data);
   };
-  const emailRegex = new RegExp(
-    "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])"
-  );
 
-  const [input, setInput] = useState('')
   const [showForm, setShowForm] = useState(true);
   const [showMessage, setShowMessage] = useState(false)
-
-  const handleInputChange = (e: any) => setInput(e.target.value)
-
-  const isError = emailRegex.test(input)
 
   const initialValues: IFormInputs = {
     firstname: '',
@@ -108,8 +100,7 @@ function ContactForm() {
                     {errors.firstname && touched.firstname && <FormHelperText>{errors.firstname}</FormHelperText>}
                   </FormControl>
                     
-
-                  
+     
                   <FormControl isRequired >
                     <FormLabel htmlFor='lastname'>Nachname</FormLabel>
                     <Field
@@ -159,7 +150,7 @@ function ContactForm() {
                     size='md'
                     type="submit"
                   >
-                      Senden
+                    Senden
                   </Button>
                   </VStack>
                 </Form>
